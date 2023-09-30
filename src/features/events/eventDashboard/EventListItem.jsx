4b34ab6ed4728211655c5842301636f5c9e303ef
Faizan Khokhar <muhammad.khokhar.97@gmail.com>
2023-09-30 21:05:33 +0500

    props

--- src/features/events/eventDashboard/EventListItem.jsx
+++ src/features/events/eventDashboard/EventListItem.jsx
@@ -2,35 +2,35 @@ import React from "react";
 import { Button, Icon, Item, List, Segment } from "semantic-ui-react";
 import EventListAttendee from "./EventListAttendee";
 
-export default function EventListItem() {
+export default function EventListItem({event}) {
   return (
     <Segment.Group>
       <Segment>
         <Item.Group>
           <Item>
-            <Item.Image size="tiny" circular src="assets/user.png" />
+            <Item.Image size="tiny" circular src={event.hostPhotoURL} />
             <Item.Content>
-              <Item.Header content="Event Title" />
-              <Item.Description>Hosted by me</Item.Description>
+              <Item.Header content={event.title} />
+              <Item.Description>{event.hostedBy}</Item.Description>
             </Item.Content>
           </Item>
         </Item.Group>
       </Segment>
       <Segment>
         <span>
-          <Icon name="clock" /> Date
-          <Icon name="map marker alternate" /> Venu
+          <Icon name="clock" /> {event.date}
+          <Icon name="map marker alternate" /> {event.venue}
         </span>
       </Segment>
       <Segment secondary>
         <List horizontal>
-          <EventListAttendee />
-          <EventListAttendee />
-          <EventListAttendee />
+          {event.attendees.map(attendee => (
+            <EventListAttendee attendee={attendee} key={attendee.id} />
+          ))}
         </List>
       </Segment>
       <Segment clearing>
-        <div>Description of event</div>
+        <div>{event.description}</div>
         <Button color="teal" floated="right" content="View" />
       </Segment>
     </Segment.Group>
